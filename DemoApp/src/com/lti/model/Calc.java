c8244ffcb345fc9173f6a0d7e8afeb87a3181eda
Aquib Ahmad <master.aquib786@gmail.com>
2020-12-18 16:07:08 +0530

    New

---
+++ DemoApp/src/com/lti/model/Calc.java
@@ -0,0 +1,11 @@
+package com.lti.model;
+
+public class Calc {
+	public void add(int n1,int n2){
+		System.out.println("Sum= "+(n1+n2));
+	}
+	
+	public void prod(int n1,int n2){
+		System.out.println("Product= "+(n1*n2));
+	}
+}
---
+++ DemoApp/src/com/lti/model/Sample.java
@@ -0,0 +1,12 @@
+package com.lti.model;
+
+public class Sample {
+	
+	public void show(){
+		System.out.println("Hello World");
+	}
+	
+	public void method(){
+		System.out.println("Hello all, how are you doing");
+	}
+}
