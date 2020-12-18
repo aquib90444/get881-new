608afa1eae28272dc56d4834ef67e9e564ad08cd
Aquib Ahmad <master.aquib786@gmail.com>
2020-12-18 16:34:31 +0530

    sub

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -5,7 +5,7 @@ public class Calc {
 		System.out.println("Sum= "+(n1+n2));
 	}
 	
-	public void prod(int n1,int n2){
-		System.out.println("Product= "+(n1*n2));
+	public void sub(int n1,int n2){
+		System.out.println("Difference= "+(n1-n2));
 	}
 }
